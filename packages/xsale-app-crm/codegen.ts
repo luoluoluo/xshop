e8ab116b21ded4d774c12701ef73611a41f179a9
luoluoluo <360646101@qq.com>
2025-08-10 15:15:07 +0800

    端口修改

--- packages/xsale-app-crm/codegen.ts
+++ packages/xsale-app-crm/codegen.ts
@@ -5,7 +5,7 @@ dotenv.config();
 const config: CodegenConfig = {
   debug: true,
   overwrite: true,
-  schema: process?.env?.VITE_API_URL || "http://localhost:3003/crm",
+  schema: process?.env?.VITE_API_URL || "http://localhost:4003/crm",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   generates: {
     "src/generated/graphql.ts": {
--- packages/xsale-app-web/codegen.ts
+++ packages/xsale-app-web/codegen.ts
@@ -5,13 +5,13 @@ dotenv.config();
 const config: CodegenConfig = {
   debug: true,
   overwrite: true,
-  schema: process?.env?.VITE_API_URL || "http://localhost:3001/web",
+  schema: process?.env?.VITE_API_URL || "http://localhost:4001/web",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   generates: {
     "src/generated/graphql.ts": {
-      plugins: ["typescript", "typescript-operations"]
-    }
-  }
+      plugins: ["typescript", "typescript-operations"],
+    },
+  },
 };
 
 export default config;
