8da3ac3813cce0a7fa58dbe67980cfd9c4ec7ba0
luoluoluo <360646101@qq.com>
2025-08-13 03:14:25 +0800

    fixbug

--- packages/xsale-app-crm/codegen.ts
+++ packages/xsale-app-crm/codegen.ts
@@ -1,11 +1,9 @@
 import { CodegenConfig } from "@graphql-codegen/cli";
-import dotenv from "dotenv";
-dotenv.config();
 
 const config: CodegenConfig = {
   debug: true,
   overwrite: true,
-  schema: process?.env?.VITE_API_URL || "http://localhost:4003/crm",
+  schema: "http://localhost:4003/crm",
   // this assumes that all your source files are in a top-level `src/` directory - you might need to adjust this to your file structure
   generates: {
     "src/generated/graphql.ts": {
